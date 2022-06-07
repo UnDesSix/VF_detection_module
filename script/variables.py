f5c55a1c86dbe34c6b62ae7e2cc5506368694682
Matthieu L <mlarboul@student.42.fr>
2022-06-07 21:23:03 +0200

    test matt

--- script/variables.py
+++ script/variables.py
@@ -1,10 +1,49 @@
 cookies = {
-
+    '__gads': 'ID=2fc6f8f2b868aa32-22c6b41738cb0056:T=1631824149:RT=1631824149:S=ALNI_MZaeDZxFX2FYSw9_4K1IJSjcphXVw',
+    '__utmz': '219780382.1637698728.94.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)',
+    'auth': 'uid=536704&secret=bWF0aXNzb3VzMTg%3D',
+    'sessid': '7097776765923823355',
+    'vfstore-adblock': '1',
+    '__gpi': 'UID=0000038d77b97331:T=1649187141:RT=1649187141:S=ALNI_MYLFrSFcZaRyT4m7zkCc7JsvDvYHA',
+    '__utma': '219780382.188256169.1631824149.1654265934.1654623138.363',
+    '__utmc': '219780382',
+    '__utmt': '1',
+    '__utmb': '219780382.29.10.1654623138',
 }
 
 headers = {
-
+    'authority': 'www.virtuafoot.com',
+    'accept': '*/*',
+    'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7',
+    'content-type': 'application/x-www-form-urlencoded',
+    # Requests sorts cookies= alphabetically
+    # 'cookie': '__gads=ID=2fc6f8f2b868aa32-22c6b41738cb0056:T=1631824149:RT=1631824149:S=ALNI_MZaeDZxFX2FYSw9_4K1IJSjcphXVw; __utmz=219780382.1637698728.94.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); auth=uid=536704&secret=bWF0aXNzb3VzMTg%3D; sessid=7097776765923823355; vfstore-adblock=1; __gpi=UID=0000038d77b97331:T=1649187141:RT=1649187141:S=ALNI_MYLFrSFcZaRyT4m7zkCc7JsvDvYHA; __utma=219780382.188256169.1631824149.1654265934.1654623138.363; __utmc=219780382; __utmt=1; __utmb=219780382.29.10.1654623138',
+    'referer': 'https://www.virtuafoot.com/',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+    'x-lang': 'fr',
+    'x-requested-with': 'XMLHttpRequest',
+    'x-sessid': '7097776765923823355',
+    'x-storage': 'sessid=7097776765923823355&auth=uid%3D536704%26secret%3DbWF0aXNzb3VzMTg%253D&vfstore-adblock=1',
 }
+
+log_file_name = 'file.log'
+
+countries_txt_path = 'list/countries.txt'
+cities_txt_path = 'list/cities.txt'
+players_txt_path = 'list/players.txt'
+
+countries_csv_path = 'csv/countries.csv'
+cities_csv_path = 'csv/cities.csv'
+players_csv_path = 'csv/players.csv'
+
+detect_nb_min = 3
+detect_nb_max = 4
 
 log_file_name = 'file.log'
 
