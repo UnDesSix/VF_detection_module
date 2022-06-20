7e7d3de289533c7805e93b0c6f9f4013d62d6bff
Matthieu L <mlarboul@student.42.fr>
2022-06-20 20:23:47 +0200

    Update variables.py

--- script/variables.py
+++ script/variables.py
@@ -54,6 +54,3 @@ players_txt_path = 'list/players.txt'
 countries_csv_path = 'csv/countries.csv'
 cities_csv_path = 'csv/cities.csv'
 players_csv_path = 'csv/players.csv'
-
-detect_nb_min = 5
-detect_nb_max = 10
